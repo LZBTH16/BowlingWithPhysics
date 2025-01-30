7e720ec77361cce18c2d9007b7d76555c5a51f26
Liz Y <lizrd2016@gmail.com>
2025-01-30 13:21:49 -0800

    Ball moves w/ player

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,6 +4,7 @@ public class BallController : MonoBehaviour
 {
     // the variables for use
     [SerializeField] private float force = 1f;
+    [SerializeField] private Transform ballAnchor;
     [SerializeField] private InputManager inputManager;
     private Rigidbody ballRB;
     private bool isBallLaunched;
@@ -17,6 +18,11 @@ public class BallController : MonoBehaviour
         // add a listener for the OnSpacePressed event
         // when the space key is pressed, the LaunchBall method will be called
         inputManager.OnSpacePressed.AddListener(LaunchBall);
+
+        // for parenting & unparenting behaviour
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
     }
 
     // the LaunchBall method
@@ -25,6 +31,9 @@ public class BallController : MonoBehaviour
         if (isBallLaunched) return;
         // if the ball is not launched, set to true & launch the ball
         isBallLaunched = true;
+        // to disconnect from the parent object
+        transform.parent = null;
+        ballRB.isKinematic = false;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 
