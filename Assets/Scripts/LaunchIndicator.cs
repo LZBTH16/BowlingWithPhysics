72660225322862ddec6361d351066bdd9f5fe9fb
Liz <lizrd2016@gmail.com>
2025-01-31 09:19:16 -0800

    Fix arrow indicator position

--- Assets/Scripts/LaunchIndicator.cs
+++ Assets/Scripts/LaunchIndicator.cs
@@ -6,10 +6,18 @@ public class LaunchIndicator : MonoBehaviour
     // the variables
     [SerializeField] private CinemachineCamera freeLookCamera;
 
+    [SerializeField] private float offset = 3f;
+
     // Update is called once per frame
     void Update()
     {
         transform.forward = freeLookCamera.transform.forward;
         transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+
+        // fixing the position of the indicator
+        Vector3 ballPosition = transform.parent.position;
+        transform.position = ballPosition + freeLookCamera.transform.forward * offset;
+        transform.position = new Vector3(transform.position.x, 1, transform.position.y);
+        // Debug.Log("arrow position: " + transform.position);
     }
 }
